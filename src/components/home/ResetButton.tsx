59cfb57064f916a7d6af5250ddcc8f5cab962f00
Butterscotch! <bscotchvanilla@gmail.com>
2022-11-01 03:01:57 -0400

    Fix reset button timer going negative

--- src/components/home/ResetButton.tsx
+++ src/components/home/ResetButton.tsx
@@ -38,16 +38,25 @@ export function ResetButton({ type }: { type: ResetType }) {
     const req = new ResetRequestT();
     req.resetType = type;
     setReseting(true);
+    setTimer(0);
     if (type !== ResetType.Quick) {
       if (timerid.current) clearInterval(timerid.current);
       timerid.current = setInterval(() => {
         setTimer((timer) => {
           const newTimer = timer + 1;
           if (newTimer >= 3) {
+            // Stop the current interval
             if (timerid.current) clearInterval(timerid.current);
-            // Only actually reset on exactly 0 so it doesn't repeatedly reset
+
+            // Only actually reset on exactly 0 so it doesn't repeatedly reset if bugged
             if (newTimer === 3) sendRPCPacket(RpcMessage.ResetRequest, req);
-            setTimer(0);
+            else
+              console.warn(
+                `Reset timer is still running after 3 seconds (newTimer = ${newTimer})`
+              );
+
+            // Reset the state
+            // Don't reset the timer in-case the interval keeps running
             setReseting(false);
           }
           return newTimer;
